58a3a6c7cbdd054b3b0826a0ea3fccb060a7c5ba
Pavithrarajendhiran123 <146984029+Pavithrarajendhiran123@users.noreply.github.com>
2024-01-27 12:03:48 +0530

    Create Capitalize.py

---
+++ 23-01-2024/Capitalize.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+def solve(s):
+    ans = s.split(' ')
+    ans1 = (((i.capitalize() for i in ans)))
+    return ' '.join(ans1)￼Enter
